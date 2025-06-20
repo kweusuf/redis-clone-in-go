faaddf1cbaae5b4316aa0c923723f6586426763f
Kanchwala, Eusuf <kweusuf@gmail.com>
2025-06-20 18:14:19 +0530

    Refactor command handling to use args array for SET, GET, DEL, INCR, DECR, INCRBY, and DECRBY; improve error handling in utility functions.

--- pkg/service/db_service.go
+++ pkg/service/db_service.go
@@ -1,10 +1,6 @@
 package service
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/kweusuf/redis-clone-in-go/pkg/model"
 	"github.com/kweusuf/redis-clone-in-go/pkg/util"
 )
@@ -33,105 +29,22 @@ func MakeDBService(store model.Store) DBService {
 func (d *dbService) HandleCommand(command string, args []string) string {
 	switch command {
 	case "SET":
-		return util.Set(&d.Store, args[0], strings.Join(args[1:], " "))
+		// return util.Set(store, args[0], strings.Join(args[1:], " "))
+		return util.Set(&d.Store, args)
 	case "GET":
-		return util.Get(&d.Store, args[0])
+		return util.Get(&d.Store, args)
 	case "DEL":
-		util.Del(&d.Store, args[0])
+		util.Del(&d.Store, args)
 		return "DELETED"
 	case "INCR":
-		return d.incr(args)
+		return util.Incr(&d.Store, args)
 	case "DECR":
-		return d.decr(args)
+		return util.Decr(&d.Store, args)
 	case "INCRBY":
-		return d.incrBy(args)
+		return util.IncrBy(&d.Store, args)
 	case "DECRBY":
-		return d.decrBy(args)
+		return util.DecrBy(&d.Store, args)
 	default:
 		return "ERROR: Unknown command"
 	}
-}
-
-// incr increments the integer value stored at the specified key by 1.
-// Returns the new value as a string, or an error message if the value is not an integer.
-func (d *dbService) incr(args []string) string {
-	if len(args) < 1 {
-		return "ERROR: INCR command requires a key"
-	}
-	valStr := util.Get(&d.Store, args[0])
-	if valStr == "" {
-		return "ERROR: key does not exist"
-	}
-	value, err := strconv.Atoi(valStr)
-	if err != nil {
-		return "ERROR: value is not an integer"
-	}
-	newValue := value + 1
-	util.Set(&d.Store, args[0], strconv.Itoa(newValue))
-	return fmt.Sprintf("%v", newValue)
-}
-
-// decr decrements the integer value stored at the specified key by 1.
-// Returns the new value as a string, or an error message if the value is not an integer.
-func (d *dbService) decr(args []string) string {
-	if len(args) < 1 {
-		return "ERROR: DECR command requires a key"
-	}
-	valStr := util.Get(&d.Store, args[0])
-	if valStr == "" {
-		return "ERROR: key does not exist"
-	}
-	value, err := strconv.Atoi(valStr)
-	if err != nil {
-		return "ERROR: value is not an integer"
-	}
-	newValue := value - 1
-	util.Set(&d.Store, args[0], strconv.Itoa(newValue))
-	return fmt.Sprintf("%v", newValue)
-}
-
-// incrBy increments the integer value stored at the specified key by the given increment.
-// Returns the new value as a string, or an error message if the value is not an integer.
-func (d *dbService) incrBy(args []string) string {
-	if len(args) < 2 {
-		return "ERROR: INCRBY command requires a key and an increment value"
-	}
-	increment, err := strconv.Atoi(args[1])
-	if err != nil {
-		return "ERROR: increment must be an integer"
-	}
-	valStr := util.Get(&d.Store, args[0])
-	if valStr == "" {
-		return "ERROR: key does not exist"
-	}
-	value, err := strconv.Atoi(valStr)
-	if err != nil {
-		return "ERROR: value is not an integer"
-	}
-	newValue := value + increment
-	util.Set(&d.Store, args[0], strconv.Itoa(newValue))
-	return fmt.Sprintf("%v", newValue)
-}
-
-// decrBy decrements the integer value stored at the specified key by the given decrement.
-// Returns the new value as a string, or an error message if the value is not an integer.
-func (d *dbService) decrBy(args []string) string {
-	if len(args) < 2 {
-		return "ERROR: DECRBY command requires a key and a decrement value"
-	}
-	decrement, err := strconv.Atoi(args[1])
-	if err != nil {
-		return "ERROR: decrement must be an integer"
-	}
-	valStr := util.Get(&d.Store, args[0])
-	if valStr == "" {
-		return "ERROR: key does not exist"
-	}
-	value, err := strconv.Atoi(valStr)
-	if err != nil {
-		return "ERROR: value is not an integer"
-	}
-	newValue := value - decrement
-	util.Set(&d.Store, args[0], strconv.Itoa(newValue))
-	return fmt.Sprintf("%v", newValue)
 }
--- pkg/util/helper.go
+++ pkg/util/helper.go
@@ -1,19 +1,125 @@
 package util
 
-import "github.com/kweusuf/redis-clone-in-go/pkg/model"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/kweusuf/redis-clone-in-go/pkg/model"
+)
 
 // Set stores the given value for the specified key in the store.
-func Set(store *model.Store, key string, value string) string {
+func Set(store *model.Store, args []string) string {
+	if len(args) < 2 {
+		return "ERROR: SET command requires a key and a value"
+	}
+	key := args[0]
+	value := args[1]
 	store.Data[key] = value
 	return value
 }
 
 // Get retrieves the value associated with the specified key from the store.
-func Get(store *model.Store, key string) string {
+func Get(store *model.Store, args []string) string {
+	if len(args) < 1 {
+		return "ERROR: GET command requires a key"
+	}
+	key := args[0]
 	return store.Data[key]
 }
 
 // Del removes the specified key and its value from the store.
-func Del(store *model.Store, key string) {
+func Del(store *model.Store, args []string) {
+	if len(args) < 1 {
+		return
+	}
+	key := args[0]
 	delete(store.Data, key)
+}
+
+// incr increments the integer value stored at the specified key by 1.
+// Returns the new value as a string, or an error message if the value is not an integer.
+func Incr(store *model.Store, args []string) string {
+	if len(args) < 1 {
+		return "ERROR: INCR command requires a key"
+	}
+	valStr := Get(store, args)
+	if valStr == "" {
+		return "ERROR: key does not exist"
+	}
+	value, err := strconv.Atoi(valStr)
+	if err != nil {
+		return "ERROR: value is not an integer"
+	}
+	newValue := value + 1
+	newArgs := []string{args[0], strconv.Itoa(newValue)}
+	Set(store, newArgs)
+	return fmt.Sprintf("%v", newValue)
+}
+
+// decr decrements the integer value stored at the specified key by 1.
+// Returns the new value as a string, or an error message if the value is not an integer.
+func Decr(store *model.Store, args []string) string {
+	if len(args) < 1 {
+		return "ERROR: DECR command requires a key"
+	}
+	valStr := Get(store, args)
+	if valStr == "" {
+		return "ERROR: key does not exist"
+	}
+	value, err := strconv.Atoi(valStr)
+	if err != nil {
+		return "ERROR: value is not an integer"
+	}
+	newValue := value - 1
+	newArgs := []string{args[0], strconv.Itoa(newValue)}
+	Set(store, newArgs)
+	return fmt.Sprintf("%v", newValue)
+}
+
+// incrBy increments the integer value stored at the specified key by the given increment.
+// Returns the new value as a string, or an error message if the value is not an integer.
+func IncrBy(store *model.Store, args []string) string {
+	if len(args) < 2 {
+		return "ERROR: INCRBY command requires a key and an increment value"
+	}
+	increment, err := strconv.Atoi(args[1])
+	if err != nil {
+		return "ERROR: increment must be an integer"
+	}
+	valStr := Get(store, args)
+	if valStr == "" {
+		return "ERROR: key does not exist"
+	}
+	value, err := strconv.Atoi(valStr)
+	if err != nil {
+		return "ERROR: value is not an integer"
+	}
+	newValue := value + increment
+	newArgs := []string{args[0], strconv.Itoa(newValue)}
+	Set(store, newArgs)
+	return fmt.Sprintf("%v", newValue)
+}
+
+// decrBy decrements the integer value stored at the specified key by the given decrement.
+// Returns the new value as a string, or an error message if the value is not an integer.
+func DecrBy(store *model.Store, args []string) string {
+	if len(args) < 2 {
+		return "ERROR: DECRBY command requires a key and a decrement value"
+	}
+	decrement, err := strconv.Atoi(args[1])
+	if err != nil {
+		return "ERROR: decrement must be an integer"
+	}
+	valStr := Get(store, args)
+	if valStr == "" {
+		return "ERROR: key does not exist"
+	}
+	value, err := strconv.Atoi(valStr)
+	if err != nil {
+		return "ERROR: value is not an integer"
+	}
+	newValue := value - decrement
+	newArgs := []string{args[0], strconv.Itoa(newValue)}
+	Set(store, newArgs)
+	return fmt.Sprintf("%v", newValue)
 }

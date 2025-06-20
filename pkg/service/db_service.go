c9a23db1eace995d9ec4cfb347666d07428cb020
Kanchwala, Eusuf <kweusuf@gmail.com>
2025-06-20 17:08:15 +0530

    Added INCR, DECR, INCRBY and DECRBY functions

--- pkg/service/db_service.go
+++ pkg/service/db_service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/kweusuf/redis-clone-in-go/pkg/model"
@@ -31,6 +33,40 @@ func (d *dbService) HandleCommand(command string, args []string) string {
 	case "DEL":
 		d.del(args[0])
 		return "DELETED"
+	case "INCR":
+		if len(args) < 1 {
+			return "ERROR: INCR command requires a key"
+		}
+		if _, err := strconv.Atoi(d.get(args[0])); err != nil {
+			return "ERROR: value is not an integer"
+		}
+		return fmt.Sprintf("%v", d.incr(args[0]))
+	case "DECR":
+		if len(args) < 1 {
+			return "ERROR: DECR command requires a key"
+		}
+		if _, err := strconv.Atoi(d.get(args[0])); err != nil {
+			return "ERROR: value is not an integer"
+		}
+		return fmt.Sprintf("%v", d.decr(args[0]))
+	case "INCRBY":
+		if len(args) < 2 {
+			return "ERROR: INCRBY command requires a key and an increment value"
+		}
+		increment, err := strconv.Atoi(args[1])
+		if err != nil {
+			return "ERROR: increment must be an integer"
+		}
+		return fmt.Sprintf("%v", d.incrBy(args[0], increment))
+	case "DECRBY":
+		if len(args) < 2 {
+			return "ERROR: DECRBY command requires a key and a decrement value"
+		}
+		decrement, err := strconv.Atoi(args[1])
+		if err != nil {
+			return "ERROR: decrement must be an integer"
+		}
+		return fmt.Sprintf("%v", d.decrBy(args[0], decrement))
 	default:
 		return "ERROR: Unknown command"
 	}
@@ -48,4 +84,28 @@ func (d *dbService) get(key string) string {
 
 func (d *dbService) del(key string) {
 	delete(d.Store.Data, key)
+}
+
+func (d *dbService) incr(key string) int {
+	value, _ := strconv.Atoi(d.get(key))
+	d.set(key, strconv.Itoa(value+1))
+	return value + 1
+}
+
+func (d *dbService) decr(key string) int {
+	value, _ := strconv.Atoi(d.get(key))
+	d.set(key, strconv.Itoa(value-1))
+	return value - 1
+}
+
+func (d *dbService) incrBy(key string, increment int) int {
+	value, _ := strconv.Atoi(d.get(key))
+	d.set(key, strconv.Itoa(value+increment))
+	return value + increment
+}
+
+func (d *dbService) decrBy(key string, decrement int) int {
+	value, _ := strconv.Atoi(d.get(key))
+	d.set(key, strconv.Itoa(value-decrement))
+	return value - decrement
 }
